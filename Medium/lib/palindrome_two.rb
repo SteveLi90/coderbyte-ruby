75be72f19dfae82568a3240f88d966fb71ec8dc0
Jayson Virissimo <jayson.virissimo@asu.edu>
2014-09-17 15:47:43 -0700

    cleaned up palindrome two

--- Medium/lib/palindrome_two.rb
+++ Medium/lib/palindrome_two.rb
@@ -1,8 +1,18 @@
 def PalindromeTwo(string)
-	cleaned_string = string.gsub(" ", "").gsub("!", "").gsub(",", "").gsub("-", "").gsub("~", "").downcase
+	cleaned_string = clean_string(string)
 	if cleaned_string == cleaned_string.reverse
 		true.to_s
 	else
 		false.to_s
 	end
+end
+
+def clean_string(string)
+	string
+		.gsub(" ", "")
+		.gsub("!", "")
+		.gsub(",", "")
+		.gsub("-", "")
+		.gsub("~", "")
+		.downcase
 end
