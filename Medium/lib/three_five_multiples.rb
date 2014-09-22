3ba95639531ee58799507f04951ac8bd4e867088
Jayson Virissimo <jayson.virissimo@asu.edu>
2014-09-21 21:32:11 -0700

    completed tests and solution for five three multiples

--- Medium/lib/three_five_multiples.rb
+++ Medium/lib/three_five_multiples.rb
@@ -1,3 +1,11 @@
 def ThreeFiveMultiples(number)
+  sum = 0
 
+  until number <= 2
+    number -= 1
+    sum += number if number % 3 == 0
+    sum += number if number % 5 == 0
+  end
+
+  sum
 end
--- Medium/spec/three_five_multiples_spec.rb
+++ Medium/spec/three_five_multiples_spec.rb
@@ -3,10 +3,16 @@ require 'three_five_multiples'
 # Return the sum of all the multiples of 3 and 5 that are below the argument.
 describe "#ThreeFiveMultiples" do
 
-  it "10 should return 23"
+  it "10 should return 23" do
+    expect(ThreeFiveMultiples(10)).to eq(23)
+  end
 
-  it "6 should return 8"
+  it "6 should return 8" do
+    expect(ThreeFiveMultiples(6)).to eq(8)
+  end
 
-  it "1 should return 0"
+  it "1 should return 0" do
+    expect(ThreeFiveMultiples(1)).to eq(0)
+  end
 
 end
