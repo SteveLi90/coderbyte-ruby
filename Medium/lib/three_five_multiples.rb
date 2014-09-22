ed327dd445a5ce1dd69a9831540b8b9dfc0f935f
Jayson Virissimo <jayson.virissimo@asu.edu>
2014-09-21 17:06:11 -0700

    created pending tests for three five multiples

---
+++ Medium/lib/three_five_multiples.rb
@@ -0,0 +1,3 @@
+def ThreeFiveMultiples(number)
+
+end
--- Medium/spec/three_five_multiples_spec.rb
+++ Medium/spec/three_five_multiples_spec.rb
@@ -3,6 +3,10 @@ require 'three_five_multiples'
 # Return the sum of all the multiples of 3 and 5 that are below the argument.
 describe "#ThreeFiveMultiples" do
 
-  
+  it "10 should return 23"
+
+  it "6 should return 8"
+
+  it "1 should return 0"
 
 end
