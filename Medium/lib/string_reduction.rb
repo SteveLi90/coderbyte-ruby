7eb0abd0cae475a2094edbb754db7eb1619603ad
Jayson Virissimo <jayson.virissimo@asu.edu>
2014-09-30 12:42:03 -0700

    deleted Hard directory

--- Medium/lib/string_reduction.rb
+++ Medium/lib/string_reduction.rb
@@ -1,22 +1,3 @@
 def StringReduction(string)
-  new_string = string
-  done = false
 
-  until done
-    done = true
-    if can_be_reduced?(new_string)
-      new_string = reduce!(new_string)
-      done = false
-    end
-  end
-
-  new_string.length
-end
-
-def can_be_reduced?(string)
-  #
-end
-
-def reduce!(string)
-  #
 end
